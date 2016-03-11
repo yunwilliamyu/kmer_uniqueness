601ed94f8b49783e5de72a9b4b52cae3fd9dedf0
Yun William Yu <ywy@mit.edu>
2016-03-11 15:19:54 -0500

    Fixed figure script up

--- figure_script.sh
+++ figure_script.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-for ((i=16; i<33; i++)); do echo ========; echo k = $i; quartz/ref_distance_variable $i hg19.fa 2>&1 | tee -a kmer_analysis.log ; done
+for ((i=16; i<65; i++))
+do
+    echo ========
+    echo k = $i
+    kmer_uniqueness/ref_distance_variable $i hg19.fa 2>&1
+    #tee -a kmer_analysis.log
+done
